f41999f155343f7e71bf99cf8c7c40e589d4ae9f
583004700 <583004700@qq.com>
2018-11-16 14:23:17 +0800

    动态sql

--- mydemo/mybatis/src/main/java/mybatis4/dynamicSQL/TestDynamicSQL.java
+++ mydemo/mybatis/src/main/java/mybatis4/dynamicSQL/TestDynamicSQL.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestDynamicSQL {
@@ -25,7 +27,7 @@ public class TestDynamicSQL {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
         Employee employee = new Employee();
-        employee.setGender("1");
+        employee.setLastName("小");
         List<Employee> list = mapper.getEmpsByConditionIf(employee);
         list.forEach((o)->System.out.println(o.getId()));
     }
@@ -38,5 +40,52 @@ public class TestDynamicSQL {
         employee.setEmail("48");
         List<Employee> list = mapper.getEmpsByConditionTrim(employee);
         list.forEach((o)->System.out.println(o.getId()));
+    }
+
+    @Test
+    public void testGetEmpsByConditionChoose(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        Employee employee = new Employee();
+        employee.setId(1);
+        List<Employee> list = mapper.getEmpsByConditionChoose(employee);
+        list.forEach((o)->System.out.println(o.getId()));
+    }
+
+    @Test
+    public void testUpdateEmp(){
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        Employee employee = new Employee();
+        employee.setGender("0");
+        employee.setId(1);
+        mapper.updateEmp(employee);
+    }
+
+    @Test
+    public void testGetEmpsByConditionForeach(){
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        Employee employee = new Employee();
+        employee.setGender("0");
+        employee.setId(1);
+        List<Employee> list = mapper.getEmpsByConditionForeach(Arrays.asList(1,4));
+        list.forEach((o)->System.out.println(o.getLastName()));
+    }
+
+    @Test
+    public void testAddEmps(){
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        Employee employee = new Employee();
+        employee.setGender("0");
+        employee.setLastName("添加1");
+        Employee employee2 = new Employee();
+        employee2.setGender("0");
+        employee2.setLastName("添加2");
+        List<Employee> emps = new ArrayList<Employee>();
+        emps.add(employee);
+        emps.add(employee2);
+        mapper.addEmps(emps);
     }
 }
