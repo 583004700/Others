1d26b17a6eab68fba84c84a5f4e08f70f015b21c
583004700 <583004700@qq.com>
2019-01-06 17:12:47 +0800

    删除mybatis项目

--- mydemo/mybatis/src/main/java/mybatis2/config/EmployeeAnnotationMapper.java
+++
@@ -1,8 +0,0 @@
-package mybatis2.config;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface EmployeeAnnotationMapper {
-    @Select("select * from tbl_employee where id = #{id}")
-    Employee testAnnotation(Integer id);
-}
--- mydemo/mybatis/src/main/java/mybatis2/config/EmployeeMapper.java
+++
@@ -1,7 +0,0 @@
-package mybatis2.config;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface EmployeeMapper {
-    Employee getEmpById(@Param("id") Integer id,@Param("lastName") Integer lastName);
-}
