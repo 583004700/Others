f9abaa32aa0cc62e633d70429676e401daebef70
583004700 <583004700@qq.com>
2019-01-13 22:41:13 +0800

    泛型

---
+++ gm2/thinkingInJava/src/zhongji/a15/generics/Holder3.java
@@ -0,0 +1,17 @@
+package zhongji.a15.generics;
+
+public class Holder3<T> {
+    private T a;
+
+    public void setA(T a){
+        this.a = a;
+    }
+
+    public T getA(){
+        return this.a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
