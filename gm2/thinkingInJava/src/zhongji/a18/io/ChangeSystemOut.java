3f6e3f215f40a54826eb56c59fa22a3df00e90b5
583004700 <583004700@qq.com>
2019-02-09 22:09:40 +0800

    gm2

---
+++ gm2/thinkingInJava/src/zhongji/a18/io/ChangeSystemOut.java
@@ -0,0 +1,11 @@
+package zhongji.a18.io;
+
+import java.io.PrintWriter;
+
+public class ChangeSystemOut {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out,true);
+        out.println("Hello, World");
+
+    }
+}
---
+++ gm2/thinkingInJava/src/zhongji/a18/io/compress/GZIPcompress.java
@@ -0,0 +1,9 @@
+package zhongji.a18.io.compress;
+
+import java.io.IOException;
+
+public class GZIPcompress {
+    public static void main(String[] args) throws IOException{
+
+    }
+}
