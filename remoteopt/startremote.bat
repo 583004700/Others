836760cd4844e180eedde8973059ca964385a10c
583004700 <583004700@qq.com>
2019-03-05 20:23:12 +0800

    删除文件

--- remoteopt/startremote.bat
+++ remoteopt/startremote.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
-start javaw -Xms215m -Xmx215m -Dfile.encoding=UTF-8 -jar remote-1.0-SNAPSHOT.jar
+start javaw -Xms215m -Xmx215m -Dfile.encoding=UTF-8 -jar remote-1.0-SNAPSHOT
 exit
