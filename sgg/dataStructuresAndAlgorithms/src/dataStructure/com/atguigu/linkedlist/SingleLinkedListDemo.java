5bd851c262ccf56f123edf0fd0a45ef03c533e34
583004700 <583004700@qq.com>
2019-06-16 08:35:24 +0800

    链表

--- sgg/dataStructuresAndAlgorithms/src/dataStructure/com/atguigu/linkedlist/SingleLinkedListDemo.java
+++ sgg/dataStructuresAndAlgorithms/src/dataStructure/com/atguigu/linkedlist/SingleLinkedListDemo.java
@@ -8,10 +8,27 @@ public class SingleLinkedListDemo {
         HeroNode heroNode4 = new HeroNode(4,"林冲","豹子头");
 
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(heroNode1);
-        singleLinkedList.add(heroNode2);
-        singleLinkedList.add(heroNode3);
-        singleLinkedList.add(heroNode4);
+        singleLinkedList.addByOrder(heroNode1);
+        singleLinkedList.addByOrder(heroNode4);
+        singleLinkedList.addByOrder(heroNode2);
+        singleLinkedList.addByOrder(heroNode3);
+
+        singleLinkedList.list();
+
+        HeroNode newHeroNode = new HeroNode(2,"小卢","玉麒麟~~");
+        singleLinkedList.update(newHeroNode);
+        System.out.println("修改后的链表情况~~");
+        singleLinkedList.list();
+
+        singleLinkedList.reverse();
+        System.out.println("反转后的链表情况~~");
+        singleLinkedList.list();
+
+        singleLinkedList.del(1);
+        singleLinkedList.del(4);
+        singleLinkedList.del(3);
+        singleLinkedList.del(2);
+        System.out.println("删除后的链表情况~~");
         singleLinkedList.list();
     }
 }
@@ -29,6 +46,82 @@ class SingleLinkedList{
             temp = temp.next;
         }
         temp.next = heroNode;
+    }
+
+    /**
+     * 添加并排序
+     * @param heroNode
+     */
+    public void addByOrder(HeroNode heroNode){
+        HeroNode temp = head;
+        boolean flag = false;
+        while(true){
+            if(temp.next == null){
+                break;
+            }
+            if(temp.next.no > heroNode.no){
+                break;
+            }else if(temp.next.no == heroNode.no){
+                flag = true;
+            }
+            temp = temp.next;
+        }
+        if(flag){
+            System.out.printf("准备插入的英雄的编号%d 已经存在了，不能加入\n",heroNode.no);
+        }else{
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+    }
+
+    //修改节点的信息，根据no编号来修改
+    public void update(HeroNode newHeroNode){
+        if(head.next == null){
+            System.out.println("链表为空~");
+            return;
+        }
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while(true){
+            if(temp == null){
+                break;
+            }
+            if(temp.no == newHeroNode.no){
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(flag){
+            temp.name = newHeroNode.name;
+            temp.nickname = newHeroNode.nickname;
+        }else{
+            System.out.printf("没有找到 编号 %d 的节点，不能修改\n",temp.no);
+        }
+    }
+
+    public void reverse(){
+        HeroNode pre = head.next;
+        HeroNode cur = pre.next;
+        pre.next = null;
+        while(cur != null){
+            HeroNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        head.next = pre;
+    }
+
+    public void del(int no){
+        HeroNode temp = head;
+        while(temp.next != null){
+            if(temp.next.no == no){
+                temp.next = temp.next.next;
+                break;
+            }
+            temp = temp.next;
+        }
     }
 
     public void list(){
