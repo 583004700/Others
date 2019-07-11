670f4313998a7cae9bf1f66e61744b2253db9dcc
583004700 <583004700@qq.com>
2019-07-12 02:59:34 +0800

    汉诺塔非递归

--- sgg/dataStructuresAndAlgorithms/src/mysources/dac/HanoitowerNoRecursion.java
+++ sgg/dataStructuresAndAlgorithms/src/mysources/dac/HanoitowerNoRecursion.java
@@ -28,37 +28,34 @@ public class HanoitowerNoRecursion {
 	}
 	
 	public void hanoiTower(int num, char a, char b, char c) {
-		paramStack.push(new Param(num,a,b,c));
-		Param param = null;
+		Param param = new Param(num,a,b,c);
+		paramStack.push(param);
 
 		while(!paramStack.empty()) {
-			param = paramStack.pop();
-			num = param.num;
 			while (true) {
-				a = param.a;
-				b = param.b;
-				c = param.c;
 				//如果只有一个盘
-				if (num == 1) {
-					System.out.println("第1个盘从 " + a + "->" + c);
+				if (param.num == 1) {
+					param = paramStack.pop();
+					System.out.println("第1个盘从 " + param.a + "->" + param.c);
 					break;
 				} else {
 					//如果我们有 n >= 2 情况，我们总是可以看做是两个盘 1.最下边的一个盘 2. 上面的所有盘
 					//1. 先把 最上面的所有盘 A->B， 移动过程会使用到 c
 					//hanoiTower(num - 1, a, c, b);
-					paramStack.push(new Param(num - 1, a, c, b));
+					param = new Param(param.num-1, param.a, param.c, param.b);
+					paramStack.push(param);
 				}
 			}
+			if(paramStack.isEmpty()){
+				break;
+			}
 			param = paramStack.pop();
-			num = param.num;
-			a = param.a;
-			b = param.b;
-			c = param.c;
 			//2. 把最下边的盘 A->C
-			System.out.println("第" + num + "个盘从 " + a + "->" + c);
+			System.out.println("第" + param.num + "个盘从 " + param.a + "->" + param.c);
 			//3. 把B塔的所有盘 从 B->C , 移动过程使用到 a塔
 			//hanoiTower(num - 1, b, a, c);
-			paramStack.push(new Param(num - 1, b, a, c));
+			param = new Param(param.num - 1, param.b, param.a, param.c);
+			paramStack.push(param);
 		}
 	}
 
