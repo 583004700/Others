1a00a5d8e33eea9be246f983b2e7903d6482b667
KK <583004700@qq.com>
2019-07-25 17:18:36 +0800

    java8

--- sgg/nio/src/source/com/atguigu/nio/day02/TestNonBlockingNIO.java
+++ sgg/nio/src/source/com/atguigu/nio/day02/TestNonBlockingNIO.java
@@ -1,12 +1,13 @@
 package source.com.atguigu.nio.day02;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -34,29 +35,31 @@ import org.junit.Test;
  */
 public class TestNonBlockingNIO {
 	
-	//客户端
-	@Test
-	public void client() throws IOException{
+	//客户端1
+	public static void main(String[] args) throws Exception{
+		c();
+	}
+
+	public static void c() throws Exception{
 		//1. 获取通道
 		SocketChannel sChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 9898));
-		
+
 		//2. 切换非阻塞模式
 		sChannel.configureBlocking(false);
-		
+
 		//3. 分配指定大小的缓冲区
 		ByteBuffer buf = ByteBuffer.allocate(1024);
-		
-		//4. 发送数据给服务端
-		Scanner scan = new Scanner(System.in);
-		
-		while(scan.hasNext()){
-			String str = scan.next();
-			buf.put((new Date().toString() + "\n" + str).getBytes());
+
+		FileInputStream fis = new FileInputStream("f:/nio.zip");
+
+		FileChannel inChannel = fis.getChannel();
+
+		while(inChannel.read(buf) != -1) {
 			buf.flip();
 			sChannel.write(buf);
 			buf.clear();
 		}
-		
+		Thread.sleep(Integer.MAX_VALUE);
 		//5. 关闭通道
 		sChannel.close();
 	}
@@ -78,6 +81,10 @@ public class TestNonBlockingNIO {
 		
 		//5. 将通道注册到选择器上, 并且指定“监听接收事件”
 		ssChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+		FileOutputStream fos = new FileOutputStream("f:/nio1.zip");
+
+		FileChannel outChannel = fos.getChannel();
 		
 		//6. 轮询式的获取选择器上已经“准备就绪”的事件
 		while(selector.select() > 0){
@@ -100,17 +107,24 @@ public class TestNonBlockingNIO {
 					//12. 将该通道注册到选择器上
 					sChannel.register(selector, SelectionKey.OP_READ);
 				}else if(sk.isReadable()){
+
 					//13. 获取当前选择器上“读就绪”状态的通道
 					SocketChannel sChannel = (SocketChannel) sk.channel();
-					
+
 					//14. 读取数据
 					ByteBuffer buf = ByteBuffer.allocate(1024);
-					
+
 					int len = 0;
 					while((len = sChannel.read(buf)) > 0 ){
 						buf.flip();
-						System.out.println(new String(buf.array(), 0, len));
+						//System.out.println(new String(buf.array(), 0, len));
+						outChannel.write(buf);
+						//System.out.println("写入文件中...");
 						buf.clear();
+					}
+					if(len == -1){
+						System.out.println("关闭文件通道");
+						outChannel.close();
 					}
 				}
 				
@@ -118,5 +132,11 @@ public class TestNonBlockingNIO {
 				it.remove();
 			}
 		}
+	}
+}
+
+class TestNonBlockingNIOClient{
+	public static void main(String[] args) throws Exception{
+		TestNonBlockingNIO.c();
 	}
 }
