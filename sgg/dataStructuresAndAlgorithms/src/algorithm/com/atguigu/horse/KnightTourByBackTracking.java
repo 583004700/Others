8fe3b94063406ff6e96efb6f34fba3cde8d0cd74
583004700 <583004700@qq.com>
2019-07-30 08:00:17 +0800

    骑士周游

--- sgg/dataStructuresAndAlgorithms/src/algorithm/com/atguigu/horse/KnightTourByBackTracking.java
+++ sgg/dataStructuresAndAlgorithms/src/algorithm/com/atguigu/horse/KnightTourByBackTracking.java
@@ -1,6 +1,6 @@
 package algorithm.com.atguigu.horse;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * 	使用回溯法(暴力匹配法)解决 骑士的周游问题。
@@ -17,6 +17,15 @@ public class KnightTourByBackTracking {
 	private int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2};
 	private int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1};
 
+	private Map<String, Set<String>> canMap = new HashMap<String,Set<String>>();
+
+	/**
+	 * 定义key的生成规则
+	 */
+	public String getKey(int x,int y){
+		return x+","+y;
+	}
+
 	/**
 	 * 构造函数，初始化棋盘和起点
 	 * 
@@ -24,6 +33,8 @@ public class KnightTourByBackTracking {
 	 * @param sourceRow    起点的行索引
 	 * @param sourceColumn 起点的列索引
 	 */
+	int lastX = -1;
+	int lastY = -1;
 	public KnightTourByBackTracking(int n, int sourceRow, int sourceColumn) {
 		if (n < 4) {
 			throw new RuntimeException("棋盘大小不能小于 4 * 4");
@@ -38,11 +49,20 @@ public class KnightTourByBackTracking {
 
 		chessboard = new int[N][N];
 		// 首先，我们让棋盘的全部元素都初始化成 -1, 表示这个位置没有遍历过
-		for (int i = 0; i < chessboard.length; i++) {
-			Arrays.fill(chessboard[i], -1);
+		for (int x = 0; x < chessboard.length; x++) {
+			for(int y = 0; y < chessboard[0].length; y++){
+				Set<String> set = new HashSet<String>();
+				for (int m = 0; m < xMove.length; m++) {
+					int nextX = x + xMove[m];
+					int nextY = y + yMove[m];
+					if (isSafe(nextX, nextY)) {
+						//表明x,y 坐标能到达哪些坐标
+						set.add(getKey(nextX,nextY));
+					}
+				}
+				canMap.put(getKey(x,y),set);
+			}
 		}
-		// 把起始点位置的元素置为 0 , 表示这个点已经遍历过，是第 0 个遍历的点
-		chessboard[sourceRow][sourceColumn] = 0;
 	}
 
 	/**
@@ -75,7 +95,15 @@ public class KnightTourByBackTracking {
 
 	int count = 0;
 
+
+
 	private void soveKT(int x, int y, int step) {
+		int oldLastX = lastX;
+		int oldLastY = lastY;
+		if(lastX!=-1){
+			//移除当前能走的路径 从上一个顶点来的
+			canMap.get(getKey(x,y)).remove(getKey(lastX,lastY));
+		}
 		chessboard[x][y] = step;
 		if (step == N * N - 1) {
 			System.out.println("已经找到"+(++count)+"种解！");
@@ -86,19 +114,21 @@ public class KnightTourByBackTracking {
 				System.out.println();
 			}
 			System.out.println("耗时：" + (System.currentTimeMillis() - start) + "毫秒");
-		}
-		int nextX = -1;
-		int nextY = -1;
-		// 然后，我们就要找点 (x, y) 旁边的全部有效点, 进行遍历
-		for (int i = 0; i < xMove.length; i++) {
-			nextX = x + xMove[i];
-			nextY = y + yMove[i];
-			// 当然，(x, y) 旁边的 8 个点不一定存在，就算存在，也不一定是空的，所以我们需要先判断一下
-			if (isSafe(nextX, nextY) && isEmpty(nextX, nextY)) {
-				soveKT(nextX, nextY, step + 1);
+		}else {
+			String key = getKey(x, y);
+			Set<String> s = canMap.get(key);
+			for (String xy : s) {
+				String[] newXy = xy.split(",");
+				int row = Integer.valueOf(newXy[0]);
+				int col = Integer.valueOf(newXy[1]);
+				lastX = x;
+				lastY = y;
+				soveKT(row, col, step + 1);
 			}
 		}
+		lastX = oldLastX;
+		lastY = oldLastY;
 		chessboard[x][y] = -1;
-		// 如果所有的相邻点都尝试过了，还是不行，我们就返回 false ， 说明此路不通
+		canMap.get(getKey(x,y)).add(getKey(lastX,lastX));
 	}
 }
