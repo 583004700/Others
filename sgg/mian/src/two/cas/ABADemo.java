37e2b862b8fd70e0c584d2fd4e6b072c4f4400d4
583004700 <583004700@qq.com>
2019-09-08 23:18:20 +0800

    springboot

--- sgg/mian/src/two/cas/ABADemo.java
+++ sgg/mian/src/two/cas/ABADemo.java
@@ -1,5 +1,6 @@
 package two.cas;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicStampedReference;
@@ -9,6 +10,8 @@ public class ABADemo {
     static AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<Integer>(100,1);
 
     public static void main(String[] args) {
+
+
         new Thread(()->{
             atomicReference.compareAndSet(100,101);
             atomicReference.compareAndSet(101,100);
@@ -25,7 +28,30 @@ public class ABADemo {
         System.out.println("=============以下是ABA问题的解决============");
 
         new Thread(()->{
-
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println(Thread.currentThread().getName()+"\t第一次版本号："+stamp);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                atomicStampedReference.compareAndSet(100,101,stamp,stamp+1);
+                System.out.println(Thread.currentThread().getName()+"\t第二次版本号："+atomicStampedReference.getStamp());
+                stamp = atomicStampedReference.getStamp();
+                atomicStampedReference.compareAndSet(101,100,stamp,stamp+1);
+                System.out.println(Thread.currentThread().getName()+"\t第三次版本号："+atomicStampedReference.getStamp());
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
         },"t3").start();
+
+        new Thread(()->{
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println(Thread.currentThread().getName()+"\t第一次版本号："+stamp);
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                boolean result = atomicStampedReference.compareAndSet(100,2019,stamp,stamp+1);
+                System.out.println("是否修改成功："+result);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        },"t4").start();
     }
 }
