20e6155832a99524638aa5aa32beb2dc88ad0ee1
KK <583004700@qq.com>
2019-09-23 11:27:59 +0800

    动态调用 去除lib

--- txkt/webservice/src/main/java/com/demo/mydemo/cxf/client/Invoke.java
+++ txkt/webservice/src/main/java/com/demo/mydemo/cxf/client/Invoke.java
@@ -1,13 +1,15 @@
 package com.demo.mydemo.cxf.client;
 
+import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
 public class Invoke {
     public static void main(String[] args) {
-        JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
-        bean.setAddress("http://localhost:8080/hihi");
+        /*JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
+        bean.setAddress("http://localhost:8081/hihi");
         bean.setServiceClass(IHelloService.class);
 
         //添加日志拦截器
@@ -16,6 +18,26 @@ public class Invoke {
         bean.getOutInterceptors().add(new AuthInfoOutInterceptor());
 
         IHelloService helloService = (IHelloService)bean.create();
-        System.out.println(helloService.sayX("小东西"));
+        System.out.println(helloService.sayX("小东西"));*/
+        main2();
+    }
+
+    public static void main2(){
+        // 创建动态客户端
+        JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+        Client client = dcf.createClient("http://localhost:8081/hihi?wsdl");
+        // 需要密码的情况需要加上用户名和密码
+        client.getOutInterceptors().add(new LoggingOutInterceptor());
+        client.getOutInterceptors().add(new LoggingInInterceptor());
+        client.getOutInterceptors().add(new AuthInfoOutInterceptor());
+        Object[] objects = new Object[0];
+        try {
+            // invoke("方法名",参数1,参数2,参数3....);
+            objects = client.invoke("sayX", "动态调用小东西");
+            System.out.println("返回数据:" + objects[0]);
+        } catch (java.lang.Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- txkt/webservice/src/main/java/com/demo/mydemo/cxf/server/Publish.java
+++ txkt/webservice/src/main/java/com/demo/mydemo/cxf/server/Publish.java
@@ -6,13 +6,13 @@ import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 public class Publish {
     public static void main(String[] args) {
         JaxWsServerFactoryBean factoryBean = new JaxWsServerFactoryBean();
-        factoryBean.setAddress("http://localhost:8080/hihi");
+        factoryBean.setAddress("http://localhost:8081/hihi");
         factoryBean.setServiceClass(IHelloService.class);
         factoryBean.setServiceBean(new HelloServiceImpl());
         //添加日志拦截器
-        factoryBean.getInInterceptors().add(new LoggingInInterceptor());
+//        factoryBean.getInInterceptors().add(new LoggingInInterceptor());
 //        factoryBean.getOutInterceptors().add(new LoggingOutInterceptor());
-        factoryBean.getInInterceptors().add(new AuthInfoInInterceptor());
+//        factoryBean.getInInterceptors().add(new AuthInfoInInterceptor());
         factoryBean.create();
         System.out.println("发布服务成功");
     }
