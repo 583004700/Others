f0166d6d7f029339083faff8558f4114c5ac434d
KK <583004700@qq.com>
2019-11-22 17:20:05 +0800

    activemq

---
+++ sgg/activeMq/src/main/java/com/atguigu/activemq/queue/JmsProduce.java
@@ -0,0 +1,34 @@
+package com.atguigu.activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+
+public class JmsProduce {
+    final static Logger LOG = LoggerFactory.getLogger(JmsProduce.class);
+
+    public static final String ACTIVEMQ_URL = "tcp://192.168.33.13:61616";
+    public static final String QUEUE_NAME = "queue01";
+
+    public static void main(String[] args) throws Exception {
+        //创建连接工厂，使用默认用户名和密码
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        LOG.info("启动成功");
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Queue queue = session.createQueue(QUEUE_NAME);
+        MessageProducer producer = session.createProducer(queue);
+        for (int i = 1; i <= 15; i++) {
+            TextMessage textMessage = session.createTextMessage("msg" + i);
+            producer.send(textMessage);
+        }
+        producer.close();
+        connection.close();
+
+        LOG.info("消息发布到MQ完成！");
+    }
+}
