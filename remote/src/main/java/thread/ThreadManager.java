c78e63709be6ec02fd14fcfee2373b6f54a845b4
KK <583004700@qq.com>
2020-04-02 20:46:11 +0800

    修改线程创建方式

--- remote/src/main/java/thread/ThreadManager.java
+++ remote/src/main/java/thread/ThreadManager.java
@@ -1,13 +1,15 @@
 package thread;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 
 public class ThreadManager {
-    private static ExecutorService executorService = Executors.newCachedThreadPool();
+    private static int threadNum = Runtime.getRuntime().availableProcessors() * 100;
+
+    private static ExecutorService executorService = new ThreadPoolExecutor(threadNum, Integer.MAX_VALUE,
+            60L, TimeUnit.SECONDS,
+            new SynchronousQueue<Runnable>());
     private static ExecutorService fileExecutorService;
-    private static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1000);
+    private static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(20);
 
     public static ExecutorService getExecutorService(){
         return executorService;
