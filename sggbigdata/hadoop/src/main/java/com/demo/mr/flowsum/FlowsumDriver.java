c14315aecb5b1667a257d4baeaabd51246739945
KK <583004700@qq.com>
2020-05-27 18:29:32 +0800

    hadoop

--- sggbigdata/hadoop/src/main/java/com/demo/mr/flowsum/FlowsumDriver.java
+++ sggbigdata/hadoop/src/main/java/com/demo/mr/flowsum/FlowsumDriver.java
@@ -1,37 +1,33 @@
-package com.demo.mr.wordcount;
+package com.demo.mr.flowsum;
 
+import com.demo.mr.wordcount.WordcountDriver;
+import com.demo.mr.wordcount.WordcountMapper;
+import com.demo.mr.wordcount.WordcountReducer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordcountDriver {
+public class FlowsumDriver {
     public static void main(String[] args) throws Exception{
         // 1 获取Job对象
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);
         // 2 设置jar存储位置
-        job.setJarByClass(WordcountDriver.class);
+        job.setJarByClass(FlowsumDriver.class);
         // 3 关联map和reduce类
-        job.setMapperClass(WordcountMapper.class);
-        job.setReducerClass(WordcountReducer.class);
+        job.setMapperClass(FlowCountMapper.class);
+        job.setReducerClass(FlowCountReducer.class);
         // 4 设置Mapper阶段输出数据的key和value类型
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(FlowBean.class);
 
         // 5 设置最终数据输出的key和value类型
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        // 如果不设置InputFormat，它默认用的是TextInputFormat.class，如果有多个小文件，每个文件都产生一个mapTask
-        job.setInputFormatClass(CombineTextInputFormat.class);
-        //虚拟存储切片最大值设置4m，多个小文件合成一次mapTask
-        CombineTextInputFormat.setMaxInputSplitSize(job, 4194304);
-
+        job.setOutputValueClass(FlowBean.class);
         // 6 设置输入路径和输出路径
         FileInputFormat.setInputPaths(job,new Path(args[0]));
         FileOutputFormat.setOutputPath(job,new Path(args[1]));
@@ -39,7 +35,5 @@ public class WordcountDriver {
         //job.submit();
         boolean result = job.waitForCompletion(true);
         System.exit(result ? 0 : 1);
-
-        //hadoop jar /bigdata/hadoop/mr.jar /wcinput /wcoutput
     }
 }
