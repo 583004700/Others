f49b1a716c1aef426f3b52c0a7c6ee8a167e3f73
583004700 <583004700@qq.com>
2020-06-15 07:56:51 +0800

    flume

--- sggbigdata/flume/src/main/java/com/demo/flume/Main.java
+++
@@ -1,4 +0,0 @@
-package com.demo.flume;
-
-public class Main {
-}
---
+++ sggbigdata/flume/src/main/java/com/demo/flume/interceptor/TypeInterceptor.java
@@ -0,0 +1,28 @@
+package com.demo.flume.interceptor;
+
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+import java.util.List;
+
+public class TypeInterceptor implements Interceptor {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Event intercept(Event event) {
+        return null;
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> list) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
